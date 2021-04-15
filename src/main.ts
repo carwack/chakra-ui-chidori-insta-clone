309b3b70ed0919b5423e26f2552c228a0624de13
carwack <sybren.willemot@gmail.com>
2021-04-15 16:51:03 +0200

    💄 extend the chakra theme

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import ChakraUIVuePlugin, { chakra } from '@chakra-ui/vue-next'
+import ChakraUIVuePlugin, { chakra, extendTheme } from '@chakra-ui/vue-next'
 import { domElements } from '@chakra-ui/vue-system'
 import {
   feBookmark,
@@ -16,6 +16,22 @@ import {
 const app = createApp(App)
 
 app.use(ChakraUIVuePlugin, {
+  extendTheme: extendTheme({
+    colors: {
+      gray: {
+        50: '#F7FAFC',
+        100: '#EDF2F7',
+        200: '#E2E8F0',
+        300: '#CBD5E0',
+        400: '#A0AEC0',
+        500: '#718096',
+        600: '#4A5568',
+        700: '#2D3748',
+        800: '#1A202C',
+        900: '#171923'
+      }
+    }
+  }),
   icons: {
     library: {
       feBookmark,
